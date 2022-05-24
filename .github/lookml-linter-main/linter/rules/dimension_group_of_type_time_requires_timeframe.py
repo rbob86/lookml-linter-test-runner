6c0b505146b5cff991e3d555fa42ab16364a1cf1
Eric Lyons <53839634+eric-lyons@users.noreply.github.com>
2022-05-24 19:39:25 +0000

    Add files via upload
    ADD LINTER

---
+++ .github/lookml-linter-main/linter/rules/dimension_group_of_type_time_requires_timeframe.py
@@ -0,0 +1,12 @@
+from linter.rule import Rule
+
+
+class DimensionGroupOfTypeTimeRequiresTimeframe(Rule):
+    def applies_to():
+        return ('dimension_group',)
+
+    def run(self, dimension_group):
+        type = dimension_group.get('type')
+        if type == 'time' and not 'timeframe' in dimension_group:
+            return False
+        return True
