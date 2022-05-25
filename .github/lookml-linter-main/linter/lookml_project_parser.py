b9afb948dee1aa3a0ab9bb0f1052f2d88213b0e3
Eric Lyons <53839634+eric-lyons@users.noreply.github.com>
2022-05-25 10:58:27 -0400

    Update lookml_project_parser.py

--- .github/lookml-linter-main/linter/lookml_project_parser.py
+++ .github/lookml-linter-main/linter/lookml_project_parser.py
@@ -16,7 +16,7 @@ class LookMlProjectParser:
         '''
         if os.path.isdir(LookMlProjectParser.root_file_path):
             lkml_filepaths = [f for f in glob.glob(
-                LookMlProjectParser.root_file_path + "**/*.lkml", recursive=True) if not(f.startswith('.'))]
+                LookMlProjectParser.root_file_path + "**/*.lkml", recursive=True) if not(f.startswith(".github"))]
         else:
             raise IOError("Directory does not exist: %s" %
                           LookMlProjectParser.root_file_path)
